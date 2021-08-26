c5099ef96f942517ba3b1a666a9aa43be11c69f3
IanGelfand <59629182+IanGelfand@users.noreply.github.com>
2021-08-26 18:11:32 -0400

    Update request-reset.js

--- server/api/request-reset.js
+++ server/api/request-reset.js
@@ -42,7 +42,7 @@ router.post('/', async (req, res, next) => {
         token: hash,
         createdAt: Date.now(),
       }).save();
-      const link = `https://iangelfand-profinance.herokuapp.com/api/users/reset-password/${user._id}/${resetToken}`;
+      const link = `https://iangelfand-profinance.herokuapp.com/reset-password/${user._id}/${resetToken}`;
       // const link = `http://localhost:3000/reset-password/${user._id}/${resetToken}`;
       if(err) console.log(err)
       if(!user) {
