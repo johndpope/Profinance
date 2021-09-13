af730631d91804d3ab2e0a9ff59e2598c023dd25
Ian Gelfand <gelfandian@gmail.com>
2021-09-13 13:48:13 -0400

    reset link

--- server/api/request-reset.js
+++ server/api/request-reset.js
@@ -42,7 +42,8 @@ router.post('/', async (req, res, next) => {
         token: hash,
         createdAt: Date.now(),
       }).save();
-      const link = `${process.env.link}/reset-password/${user._id}/${resetToken}`;
+      const link = process.env.link === 'http://localhost:3000' ? process.env.link : 'https://iangelfand-profinance.herokuapp.com'
+      // const link = `${process.env.link}/reset-password/${user._id}/${resetToken}`;
       if(err) console.log(err)
       if(!user) {
         console.log('no user') 
